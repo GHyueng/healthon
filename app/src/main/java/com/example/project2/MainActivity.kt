3d46e0501c149cd8cfeaa91aedb3b5a38c746c90
GHyueng <144517683+GHyueng@users.noreply.github.com>
2023-11-08 19:57:03 +0900

    업데이트 0.1.0
    - 메인 메뉴, 식단, 그룹 프래그먼트 생성
    - 메인메뉴, 식단, 그룹 하단바 생성

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -1,16 +1,57 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.project2.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.diet -> replaceFragment(Diet())
+                R.id.group -> replaceFragment(Group())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
     }
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+
+
+
 }
 
-//hello 여빈
-//hello 근형
-//hello 찬혁
+
+
 
