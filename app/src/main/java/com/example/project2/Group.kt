943d4db5a337d97f83e0f7b66821d14a995a98ce
Codekookie <ri7116@naver.com>
2023-11-15 00:38:17 +0900

    Revert "취소 ㄱㄱ 첨부터함"
    This reverts commit ad281ec67830b64af5005bcca77664bdb6e16b1d.

--- app/src/main/java/com/example/project2/Group.kt
+++ app/src/main/java/com/example/project2/Group.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.fragment.findNavController
-import com.example.project2.databinding.FragmentFriendsBinding
-import com.example.project2.databinding.FragmentGroupBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -16,22 +17,43 @@ import com.example.project2.databinding.FragmentGroupBinding
  * create an instance of this fragment.
  */
 class Group : Fragment() {
-    var binding: FragmentGroupBinding? = null
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding= FragmentGroupBinding.inflate(inflater)
-        return binding?.root
-    }
-    //생성 후 작업은 여기서
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)// 기본
-
-        binding?.groupbtn1?.setOnClickListener{
-            findNavController().navigate(R.id.action_group2_to_friends)
-        }
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group, container, false)
     }
 
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Group.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Group().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
